9344a3c013615f7a123f5835fc30167bdba237d0
chems <ic_benaziza@esi.dz>
2023-04-24 23:14:40 +0200

    landing page working

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express")
-const { default: test } = require("node:test")
-const app = express()
+const mongoose = require("mongoose")
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 
-app.set('view engine', 'ejs')
+const app = express()
 
-app.use('/articles', articleRouter)
+mongoose.set('strictQuery', true);
+// connection de la base de données
+mongoose.connect('mongodb://127.0.0.1/blog', {
+    useNewUrlParser: true,useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('connected to database')
+    }
+})
+
+app.set('view engine', 'ejs')
+// this line is used to get the data from the form
+app.use(express.urlencoded({ extended: false }))
+
+
+
 
 app.get('/', (req, res) => {
 
@@ -23,5 +40,6 @@ app.get('/', (req, res) => {
 
     res.render('articles/index', { articles: articles });
 })
+app.use('/articles', articleRouter)
 
 app.listen(5000)
