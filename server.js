b73e94fee459fa685caef21b4543280a558b893c
chems benaziza <chems.benaziza@gmail.com>
2023-04-25 18:01:46 +0200

    added navbar and changed database

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const sqlite3 = require('sqlite3').verbose() 
+// verbose to produce long stack trace
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 
 const app = express()
-
-//mongoose.set('strictQuery', true);
-// connection de la base de données
-/*mongoose.connect('mongodb://127.0.0.1/blog', {
+/*
+mongoose.set('strictQuery', true);
+//connection de la base de données
+mongoose.connect('mongodb://127.0.0.1/blog', {
     useNewUrlParser: true, useUnifiedTopology: true
 }, (err) => {
     if (err) {
@@ -16,6 +18,19 @@ const app = express()
         console.log('connected to database')
     }
 })*/
+
+let db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+});
+db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
 
 app.set('view engine', 'ejs')
 // this line is used to get the data from the form
